49921e655bf40ddff15e3878ce7f702cb0095b68
svnjj <96128553+svnjj@users.noreply.github.com>
2021-12-14 15:49:30 +0100

    Füge das Dokument hinzu

---
+++ BMI.py
@@ -0,0 +1,4 @@
+#Dieses Programm rechnet aufgrund des Gewichts und der Grösse den BMI aus.
+x=float(input("Gib dein Gewicht in kg ein: "))#x ist die Eingabe des Gewichts
+y=float(input("Gibt deine Grösse in m ein: "))#y ist die Eingabe der Grösse
+print("Dein BMI ist: "+ str(x/y))#Der BMI ist das Gewicht geteilt durch die Grösse und wird als Ausgabe angezeigt.
